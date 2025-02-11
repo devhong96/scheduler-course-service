2f4531f591b8dbb04172bb41493ab6df03779300
devhong96 <dev.hong96@gmail.com>
2025-02-11 13:19:43 +0900

    jwt

--- src/main/java/com/scheduler/courseservice/client/MemberServiceClient.java
+++ src/main/java/com/scheduler/courseservice/client/MemberServiceClient.java
@@ -2,17 +2,25 @@ package com.scheduler.courseservice.client;
 
 import com.scheduler.courseservice.infra.error.MemberFeignErrorDecoder;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 
 import static com.scheduler.courseservice.client.dto.FeignMemberInfo.*;
 
 @FeignClient(name = "member-service", configuration = MemberFeignErrorDecoder.class)
 public interface MemberServiceClient {
 
+    @PostMapping("{studentName}/{password}")
     StudentInfo findCourseByStudentName(String studentName, String password);
 
-    TeacherInfo findTeacherByStudentId(String studentId);
+    @GetMapping("{studentId}")
+    TeacherInfo findTeacherByStudentId(@PathVariable String studentId);
 
-    MemberInfo findMemberInfoByToken(String token);
+    @GetMapping("memberInfo")
+    MemberInfo findMemberInfoByToken(@RequestHeader("Authorization") String token);
 
+    @PostMapping("{studentName}/{password}")
     StudentInfo findStudentByStudentNameAndPassword(String studentName, String password);
 }
