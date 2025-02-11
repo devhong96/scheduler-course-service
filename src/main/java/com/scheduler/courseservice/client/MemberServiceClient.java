7618df3449ad45281717d3a750d3f3ad0b3c4249
devhong96 <dev.hong96@gmail.com>
2025-02-11 12:42:45 +0900

    feign

--- src/main/java/com/scheduler/courseservice/client/MemberServiceClient.java
+++ src/main/java/com/scheduler/courseservice/client/MemberServiceClient.java
@@ -1,10 +1,11 @@
 package com.scheduler.courseservice.client;
 
+import com.scheduler.courseservice.infra.error.MemberFeignErrorDecoder;
 import org.springframework.cloud.openfeign.FeignClient;
 
 import static com.scheduler.courseservice.client.dto.FeignMemberInfo.*;
 
-@FeignClient
+@FeignClient(name = "member-service", configuration = MemberFeignErrorDecoder.class)
 public interface MemberServiceClient {
 
     StudentInfo findCourseByStudentName(String studentName, String password);
