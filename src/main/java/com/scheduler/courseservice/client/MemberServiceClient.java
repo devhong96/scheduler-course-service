2ca1d19ca139494f47767330e6451ec6f5bdeaf0
devhong96 <dev.hong96@gmail.com>
2025-03-14 11:04:16 +0900

    debug : feign

--- src/main/java/com/scheduler/courseservice/client/MemberServiceClient.java
+++ src/main/java/com/scheduler/courseservice/client/MemberServiceClient.java
@@ -2,6 +2,7 @@ package com.scheduler.courseservice.client;
 
 import com.scheduler.courseservice.infra.error.MemberFeignErrorDecoder;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 
@@ -20,7 +21,7 @@ public interface MemberServiceClient {
             @RequestHeader(AUTHORIZATION) String token
     );
 
-    @PostMapping("/feign-member/student/info")
+    @GetMapping("/feign-member/student/info")
     StudentInfo findStudentInfoByToken(
             @RequestHeader(AUTHORIZATION) String token
     );
