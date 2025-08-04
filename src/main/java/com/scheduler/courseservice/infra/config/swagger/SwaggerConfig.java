f6225a207effb2288ab9414bd5e3a6a155ab862d
devhong96 <dev.hong96@gmail.com>
2025-08-04 15:50:41 +0900

    modify: swagger

--- src/main/java/com/scheduler/courseservice/infra/config/swagger/SwaggerConfig.java
+++ src/main/java/com/scheduler/courseservice/infra/config/swagger/SwaggerConfig.java
@@ -1,16 +1,20 @@
 package com.scheduler.courseservice.infra.config.swagger;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.info.Info;
-import lombok.RequiredArgsConstructor;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@OpenAPIDefinition(
-        info = @Info(title = "Course Service Api")
-)
 @Configuration
-@RequiredArgsConstructor
 public class SwaggerConfig {
 
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info().title("Course Service Api").version("1.0"))
+                .addServersItem(new Server().url("/scheduler-course-service").description("API Gateway URL"))
+                .addServersItem(new Server().url("/").description("Local DEV Server"));
+    }
 
 }
